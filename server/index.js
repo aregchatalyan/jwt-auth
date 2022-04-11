b9cf1f831982b3cb2846dee5d0f20605e1004e74
MelqonHovhannisyan <melkon.hovhannisian@gmail.com>
2022-04-11 18:14:07 +0400

    first

---
+++ server/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+const router = require('./router/index');
+const errorMiddleware = require('./middlewares/error-middleware');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
+
+app.use('/api', router);
+app.use(errorMiddleware);
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start().then();
